8d180d7bef04d0dcfc1aa896c50cea14fd0aee5e
G_Yaswanth# <133037871+GYaswanth030604@users.noreply.github.com>
2026-02-25 14:19:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -346,4 +346,5 @@ if __name__ == "__main__":
     start_services()
 
     port = int(os.environ.get("PORT", 10000))
-    socketio.run(app, host="0.0.0.0", port=port, debug=False)
+    socketio.run(app, host="0.0.0.0", port=port, allow_unsafe_werkzeug=True)
+
